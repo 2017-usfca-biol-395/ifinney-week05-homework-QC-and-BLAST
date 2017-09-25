1c7fe508e0c0aa1405f563483b140aa3ee7966e9
ifinney <ifinney@dons.usfca.edu>
2017-09-25 19:55:04 +0000

    Add trim to fastq files

--- blast_pipeline.sh
+++ blast_pipeline.sh
@@ -5,18 +5,20 @@ if [ -z ${BLASTDB} ]; then source /home/.bashrc; fi
 
 for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
 do
-	fastq-dump -v $SRA_number -O data/raw_data
+	echo fastq-dump -v $SRA_number -O data/raw_data
 done
 
 # This runs QC reports
-fastqc data/raw_data/*.fastq --outdir=output/fastqc
+echo fastqc data/raw_data/*.fastq --outdir=output/fastqc
 
-#for file in $@
-#do 
-#	TrimmomaticSE -threads 2 -phred33 data/raw_data/ERR1942280.fastq data/trimmed/$(basename -s .fastq ERR1942280.fastq).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
-#done
+echo mkdir data/trimmed_seq
 
-#for a file in $@
+for file in data/raw_data/*.fastq
+do 
+	TrimmomaticSE -threads 2 -phred33 $file data/trimmed/$(basename -s .fastq $file).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
+done
+
+#for a file in 
 #do
 #	bioawk -c fastx '{print ">"$name"\n"$seq}' data/trimmed/filename.trim.fastq
 #done
